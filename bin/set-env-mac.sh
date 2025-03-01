9fb690bd43cccc685560d0a477d85f221160075b
Janesh Kodikara <janesh.kodikara@gmail.com>
2025-03-02 02:54:24 +0530

    Add script file to set JAVA_HOME and PATH temporarily

---
+++ bin/set-env-mac.sh
@@ -0,0 +1,3 @@
+# Set JAVA_HOME and PATH in Command Prompt (Temporary)
+export JAVA_HOME="/opt/homebrew/opt/openjdk@11"
+export PATH="$JAVA_HOME/bin:$PATH"
---
+++ bin/set-env-win.bat
@@ -0,0 +1,4 @@
+@REM Set JAVA_HOME and PATH in Command Prompt (Temporary)
+@REM Replace C:\Program Files\Java\jdk-11 with the actual path
+set JAVA_HOME=C:\Program Files\Java\jdk-11
+set PATH=%JAVA_HOME%\bin;%PATH%
