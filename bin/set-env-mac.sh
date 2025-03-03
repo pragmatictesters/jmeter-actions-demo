494734094922fdbec8fb610646f48aee77693f46
Janesh Kodikara <janesh.kodikara@gmail.com>
2025-03-03 11:30:20 +0530

    Change the JDK version

--- bin/set-env-mac.sh
+++ bin/set-env-mac.sh
@@ -1,3 +1,3 @@
 # Set JAVA_HOME and PATH in Command Prompt (Temporary)
-export JAVA_HOME="/opt/homebrew/opt/openjdk@11"
+export JAVA_HOME="/opt/homebrew/opt/openjdk@17"
 export PATH="$JAVA_HOME/bin:$PATH"
